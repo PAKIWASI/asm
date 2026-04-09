a9142c3ec16337429e8d3936475098df944080c4
PAKIWASI <ullahwasi26@gmail.com>
2026-04-09 12:33:24 +0500

    strcmp

---
+++ strcmp/strcmp.asm
@@ -0,0 +1,81 @@
+
+
+section .data
+
+    newline db 0x0a
+
+    msg_1 db "please enter first string: ", 0x0a, 0 ; newline + null terminated
+    msg_1_len equ $ - msg_1
+
+    msg_2 db "please enter second string: ", 0x0a, 0
+    msg_2_len equ $ - msg_2
+
+
+
+section .bss
+    str_1 resb 256  ; resb: reserve byte
+    str_2 resb 256
+
+
+section .text
+    global _start
+
+
+; rdi: buffer ptr
+; rsi: buffer len
+print_str:
+    push rbp
+    mov  rbp, rsp
+       
+    ; sys_write: rax (sys call no) = 1, rdi (fd, stdout = 1) = 1, rsi = buf, rdx = len
+    mov  rsi, rdi    ; buff
+    mov  rdx, rsi    ; len     
+    mov  rdi, 1      ; stdout
+    mov  rax, 1      ; syswrite
+    syscall
+
+    pop  rbp
+    ret
+
+; rdi: buffer ptr
+read_str:
+    push rbp
+    mov rbp, rsp
+
+    mov rsi, rdi
+    mov rdx, 256    ; buffer len
+    mov rdi, 0      ; stdin
+    mov rax, 0      ; read syscall
+    syscall
+
+    pop rbp
+    ret
+
+_start:
+    push rbp    
+    mov rbp, rsp
+
+
+    mov rdi, msg_1
+    mov rsi, msg_1_len
+    call print_str
+
+    mov rdi, str_1
+    call read_str
+
+
+    mov rdi, msg_2
+    mov rsi, msg_2_len
+    call print_str
+
+    mov rdi, str_2
+    call read_str
+
+
+; this runs automatically
+program_end:
+    mov rax, 60     ; exit syscall
+    mov rdi, 0      ; normal exit
+    syscall
+
+
